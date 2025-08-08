68869a85b9afb95140d2f3e50da15f6d4c942e63
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-08-08 07:09:02 -0400

    Initial commit

---
+++ be-enhancing.js
@@ -0,0 +1,66 @@
+// @ts-check
+import { propInfo, rejected, resolved } from 'be-enhanced/cc.js';
+import { BE } from 'be-enhanced/BE.js';
+import {dispatchEvent as de} from 'trans-render/positractions/dispatchEvent.js';
+/** @import {BEConfig, IEnhancement, BEAllProps} from './ts-refs/be-enhanced/types.d.ts' */
+/** @import {Actions, ProPAP, PAP, AllProps, AP, BAP} from './ts-refs/be-enhancing/types' */
+
+/**
+ * @implements {Actions}
+ * 
+ */
+class BeEnhancing extends BE {
+    /**
+     * @type {BEConfig<AP & BEAllProps, Actions & IEnhancement>}
+     */
+    static config = {
+        propInfo: {
+            ...propInfo,
+            parsedStatements: {},
+            rawStatements: {},
+        },
+        positractions: [resolved, rejected],
+        compacts:{
+            when_parsedStatements_changes_call_hydrate: 0,
+        }
+    }
+
+    de = de;
+
+    /**
+     * 
+     * @param {BAP} self 
+     * @returns 
+     */
+    async hydrate(self){
+        const {parsedStatements, enhancedElement} = self;
+        const {find} = await import('trans-render/dss/find.js');
+        const {whenResolved} = await import('be-hive/whenResolved.js');
+        for(const parsedStatement of parsedStatements){
+            const {localSpecifier, remoteSpecifier, remotePropertyPath, enhancementBase} = parsedStatement;
+            const remoteEl = await find(enhancedElement, remoteSpecifier);
+            if(!remoteEl) throw 404;
+            
+            const enhancement = await whenResolved(remoteEl, enhancementBase);
+            const {path} = localSpecifier;
+            if(path === undefined) continue;
+            if(remotePropertyPath === undefined){
+                (await import('trans-render/lib/setProp.js')).setProp(enhancedElement, path, enhancement);
+            }else{
+                new (await import('trans-render/asmr/BeLinked.js')).BeLinked(
+                    enhancement, remotePropertyPath, enhancedElement, path
+                );
+
+            }
+            
+        }
+        return /** @type {PAP} */({
+            resolved: true,
+        })
+    }
+
+
+}
+
+await BeEnhancing.bootUp();
+export { BeEnhancing };
---
+++ emc.js
@@ -0,0 +1,48 @@
+// @ts-check
+import { BeHive, MountObserver, seed } from 'be-hive/be-hive.js';
+/** @import {AP} from './ts-refs/be-enhancing/types'; */
+/** @import {EMC} from './ts-refs/trans-render/be/types' */
+
+const remoteSpecifierStringSetLocalProp = 
+    String.raw `^(?<remoteSpecifierString>.*) with (?<enhancementBase>.*) and set (?<localSpecifierString>.*)`;
+
+const remoteSpecifierStringSetLocalPropToRemoteProp = String.raw `${remoteSpecifierStringSetLocalProp} to (?<remotePropertyPath>.*)`;
+
+/**
+ * @type {[string, string]}
+ */
+const rssTors = ['remoteSpecifierString', 'remoteSpecifier'];
+/**
+ * @type {EMC<any, AP>}
+ */
+export const emc = {
+    base: 'be-enhancing',
+    enhPropKey: 'beEnhancing',
+    importEnh: async () => {
+        const { BeEnhancing } = await import('./be-enhancing.js');
+        return BeEnhancing;
+    },
+    map: {
+        '0.0': {
+            instanceOf: 'Object$entences',
+            objValMapsTo: '.',
+            regExpExts: {
+                parsedStatements: [
+                    {
+                        regExp: remoteSpecifierStringSetLocalPropToRemoteProp,
+                        defaultVals: {},
+                        dssKeys: [['remoteSpecifierString', 'remoteSpecifier'], ['localSpecifierString', 'localSpecifier']],
+                    },
+                    {
+                        regExp: remoteSpecifierStringSetLocalProp,
+                        defaultVals: {},
+                        dssKeys: [['remoteSpecifierString', 'remoteSpecifier'], ['localSpecifierString', 'localSpecifier']],
+                    }
+                ]
+            }
+        }
+    }
+};
+
+const mose = seed(emc);
+MountObserver.synthesize(document, BeHive, mose);
